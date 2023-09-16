b5320139df1e07ea9c7d11c71df65cf9f7e76b24
Hoang Ngoc Phu <jerryhoang.kaka1901@gmail.com>
2023-09-16 22:17:17 +0700

    update my testbench, with sending 0x7f and 0x80 consecutively

--- i2c_slave/sim/i2c_tb.v
+++ i2c_slave/sim/i2c_tb.v
@@ -1,45 +1,100 @@
-`timescale 1ns/10ps
+`timescale 1ns/1ps
 
 module i2c_tb();
-    reg SCL, clk, rst, bit_en;
-    wire SDA;
+    reg SCL_in, SDA_in, clk, rst, master_2_slave;
+    wire SDA_input;
+    //wire bit_en;
     reg [7:0] DATA_IN;
     wire [7:0] DATA_OUT;
-    assign SDA = bit_en ? 1'b1 : 1'b0;
-    
-    i2c UUT (.clk(clk), .SCL(SCL), .SDA(SDA), .DATA_IN(DATA_IN), .DATA_OUT(DATA_OUT), .rst(rst));
+    //assign SDA_in = bit_en ? 1'b1 : 1'b0;
+    assign SDA_input = master_2_slave ? SDA_in : 1'bz;
+    i2c UUT (.sys_clk(clk), .SCL_in(SCL_in), .SDA_in(SDA_input), .DATA_IN(DATA_IN), .DATA_OUT(DATA_OUT), .sys_rst_n(rst));
+    initial DATA_IN = 8'b1;
     initial begin
         clk = 0;
         forever begin
-            clk = #1 !clk;
+            clk = #0.02 !clk;
         end
         
     end
     
     initial begin
-        rst = 0;
-        #0.2 rst = 1;
+        rst = 1;
         #0.2 rst = 0;
+        #0.2 rst = 1;
     end
     
     initial begin
-        SCL = 1;
+        SCL_in = 1;
         forever begin
-            #10 SCL = ~SCL;
+            #5 SCL_in = ~SCL_in;
         end
     end
     
     initial begin
-        bit_en = 1;
-        #5 bit_en = 0;
-        #134 bit_en = 1;
-        #20 bit_en = 0;
-        #38 bit_en = 1;
-        #160 bit_en = 0;
-        #20 bit_en = 1;
-        
-        
+//        WRITE_MODE      
+//        master_2_slave = 1;
+//        SDA_in = 1;
+//        #2;
+//        SDA_in = 0; //address pt1
+//        #67;
+//        SDA_in = 1; //address pt2
+//        #10;
+//        SDA_in = 0; //read write
+//        #10;
+//        SDA_in = 1;
+//        #90;
+//        SDA_in = 0;
+//        #10;
+//        SDA_in = 0;
+//        #3;
+//        SDA_in = 1;
 
+//        READ_MODE
+        master_2_slave = 1;
+        SDA_in = 1;
+        #2;
+        SDA_in = 0; //address pt1
+        #67;
+        SDA_in = 1; //address pt2
+        #7;
+        SDA_in = 0; //read write
+        #10;
+        master_2_slave = 0;
+        #10;
+        master_2_slave = 1;
+        SDA_in = 0;
+        #10;
+        SDA_in = 1;
+        #70;
+        SDA_in = 0;
+        #10;
+        SDA_in = 0;
+        #3;
+        SDA_in = 1;
+        
+        #41;
+        master_2_slave = 1;
+        SDA_in = 1;
+        #2;
+        SDA_in = 0; //address pt1
+        #67;
+        SDA_in = 1; //address pt2
+        #7;
+        SDA_in = 0; //read write
+        #10;
+        master_2_slave = 0;
+        #10;
+        master_2_slave = 1;
+        SDA_in = 1;
+        #10;
+        SDA_in = 0;
+        #70;
+        SDA_in = 0;
+        #10;
+        SDA_in = 0;
+        #3;
+        SDA_in = 1;
     end
 
     
